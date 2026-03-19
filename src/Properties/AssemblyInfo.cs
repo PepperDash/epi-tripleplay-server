45e3fbbd9953217f3ec037987703fe819b3ec04e
Colin Denig <cdenig@pepperdash.com>
2026-03-19 14:09:32 -0400

    chore: rename project directory to src chore: move sln file to root dir

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("PepperDash.Essentials.Plugin.TriplePlay.IptvServer")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PepperDash.Essentials.Plugin.TriplePlay.IptvServer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildType-build#")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildType-build#")]
+
